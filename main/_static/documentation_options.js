31d5a799f7d4cd438b28b3451c425a0fd512c962
JoshuaAppleby <JoshuaAppleby@users.noreply.github.com>
2023-01-23 15:54:58 +0000

    deploy: 3a29b47aa01f842cb418554a4634eb758a5ebfc0

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev42+g2f33c2c',
+    VERSION: '0.1.dev66+g3a29b47',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
