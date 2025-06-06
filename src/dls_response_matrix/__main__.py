91123ac5151b0b3e85809e223fc912edd944ea32
Phil Smith <pts@observatorysciences.co.uk>
2025-06-06 12:45:54 +0000

    Add filepath as CLI argument

--- src/dls_response_matrix/__main__.py
+++ src/dls_response_matrix/__main__.py
@@ -9,6 +9,7 @@ __all__ = ["main"]
 
 HELP_INFO = {
     "filename": "The filename for the saved files. Default is the ISO time.",
+    "filepath": "The path to the directory where the files should be saved. Default is cwd",
     "ring-mode": "The ring mode of the model. Default is I04",
     "proposed-delta": "The proposed delta to vary correctors by.",
     "pytac-unit": (
@@ -28,8 +29,16 @@ def parse_arguments():
     parser.add_argument(
         "--filename",
         "-f",
+        type=str,
         default=None,
         help=HELP_INFO["filename"],
+    )
+    parser.add_argument(
+        "--filepath",
+        "-p",
+        type=str,
+        default=None,
+        help=HELP_INFO["filepath"],
     )
     parser.add_argument(
         "--ring-mode",
@@ -86,6 +95,7 @@ def main(args=None):
     args = parse_arguments()
     response_matrix(
         args.filename,
+        args.filepath,
         args.ring_mode,
         args.proposed_delta,
         args.pytac_unit,
